10c22dd5b7934a463953c7a04879fd07a445ca9e
ngan <nttn.love.waffle@gmail.com>
2021-10-05 20:52:04 +0700

    change database

--- cooler_hcmue/settings.py
+++ cooler_hcmue/settings.py
@@ -82,24 +82,31 @@ WSGI_APPLICATION = 'cooler_hcmue.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#
+#     'default': {
+#
+#         'ENGINE': 'django.db.backends.postgresql',
+#
+#         'NAME': 'ecommerce',
+#
+#         'USER': 'postgres',
+#
+#         'PASSWORD': config('DB_PWD'),
+#
+#         'HOST': 'localhost',
+#
+#         'PORT': '5432',
+#
+#     }
+#
+# }
+
 DATABASES = {
-
     'default': {
-
-        'ENGINE': 'django.db.backends.postgresql',
-
-        'NAME': 'ecommerce',
-
-        'USER': 'postgres',
-
-        'PASSWORD': config('DB_PWD'),
-
-        'HOST': 'localhost',
-
-        'PORT': '5432',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-
 }
 
 CACHES = {
